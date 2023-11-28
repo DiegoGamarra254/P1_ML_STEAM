03b2fb28aaf873e3f9a59b2e64e6b05e1e202635
Diego Gamarra Rivera <135669863+DiegoGamarra254@users.noreply.github.com>
2023-11-27 20:09:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,51 +12,6 @@ from typing import List, Dict, Tuple, Sequence, Any, Union, Optional, Callable
 
 
 
-'''app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"mensaje": "BIENVENIDOS A LA API DEL PROYECTO 1, Realizado por Diego Gamarra Rivera"}
-
-
-# Endpoint para PlayTimeGenre
-@app.get("/PlayTimeGenre/{genero}")
-async def user(genero: str):
-    result = PlayTimeGenre(genero)
-    return result
-
-# Endpoint para UserForGenre
-@app.get("/userforgenre/{genero}")
-def read_user_for_genre(genero: str):
-    result = UserForGenre(genero)
-    return result
-
-# Endpoint para UsersRecommend
-@app.get("/usersrecommend/{year}")
-def read_users_recommend(year: int):
-    result = UsersRecommend(year)
-    return result
-
-# Endpoint para UsersWorstDeveloper
-@app.get("/usersworstdeveloper/{year}")
-def read_users_worst_developer(year: int):
-    result = UsersWorstDeveloper(year)
-    return result
-
-# Endpoint para sentiment_analysis
-@app.get("/sentiment/{developer}")
-def read_sentiment(developer: str):
-    result = sentiment_analysis(developer)
-    return result
-
-# Endpoint para recomendacion_juego
-@app.get("/recomendacion_juego/{id}")
-def user(id: int):
-    result = recomendacion_juego(id)
-    return result'''
-
-from fastapi.responses import HTMLResponse
-
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
